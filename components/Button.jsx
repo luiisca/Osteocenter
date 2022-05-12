1d321eae37714f7a774c3eed80f7d086d9552f15
Luis Cadillo <lusicaf3r@Gmail.com>
2022-05-11 21:34:49 -0500

    CONFIG: add global styling and normalizer.css

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,27 +1,29 @@
-import styled, {css} from 'styled-components';
+import tw, {css, styled} from 'twin.macro';
 
-const Button = styled.button({
-  backgroundColor: 'red',
-});
-
-// const Button = tw.button`
-//   flex
-//   inline-flex
-//   items-center
-//   border
-//   border-transparent
-//   text-xs
-//   font-medium
-//   rounded-md
-//   shadow-md
-//   text-white
-//   p-2
-//   m-2
-//   transition-all
-
-//   hover:bg-indigo-500
-//   ${props => props.primary ? 'bg-indigo-400' : 'bg-indigo-300'}
-//  `
+const Button = styled.button(props => [
+  css`
+    background-color: salmon;
+    &:hover {
+    background-color: #ff0000;
+    };
+  `,
+  tw`text-xs font-medium text-white`,
+  tw`flex inline-flex w-full`,
+  tw`border border-transparent`,
+  tw`p-2 m-2`,
+  tw`items-center rounded-md`,
+  tw`shadow-md`,
+  // tw`hover:bg-indigo-500`,
+  props.primary ? tw`bg-indigo-400` : 'bg-indigo-300'
+]);
+// const Button = styled.button({
+//   backgroundColor: 'salmon',
+//   border: 'none',
+//   ...tw`text-xs font-medium text-purple-500`,
+//   '&:hover': {
+//     ...tw`bg-indigo-500`,
+//   },
+// })
 
 export default Button;
 
