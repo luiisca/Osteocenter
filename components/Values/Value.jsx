dc93c6252927c91e64c654263f0783ed8d690ff8
Luis Cadillo <lusicaf3r@Gmail.com>
2022-05-17 12:53:53 -0500

    STYLING: Add Values component

---
+++ components/Values/Value.jsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import tw, {css, styled} from 'twin.macro';
+
+import Heading from '../Heading';
+
+const Container = tw.div`flex flex-col items-center`
+const ImgWrap = styled.div`
+    ${tw`relative max-w-[5rem]`}
+    &::before {
+      content: '';
+      ${tw`block absolute top-[20%] z-[-1] w-4/5 h-4/5 bg-no-repeat bg-contain`}
+      background-image: url('../../static/img/shapes/${props => props.position}-bubble.png');
+    }
+  `
+const Text = tw.p`text-lg max-w-[30ch]`
+
+const Value = ({name, shapePosition}) => {
+  return (
+    <Container>
+      <ImgWrap position={shapePosition} tw='mb-8'>
+        <Image
+          src={`/img/icons/${name}.svg`}
+          alt={`${name} logo`}
+          layout='raw'
+          size='10vw'
+          width='500'
+          height='500'
+        />
+      </ImgWrap>
+      <Heading as='h3' tertiary tw='mb-4 font-sans'>
+        <span>{name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}</span>
+      </Heading>
+      <Text>
+        lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </Text>
+    </Container >
+  )
+}
+
+export default Value
--- components/containers/main.jsx
+++ components/containers/main.jsx
@@ -1,10 +1,12 @@
 import tw, {css, styled} from 'twin.macro';
 import {Fragment} from "react";
 import Hero from '../Hero';
+import Values from '../Values';
 
 const Section = styled.section(props => [
-  tw`px-24`,
-  props.hero && tw`bg-primary-tint-3`
+  tw`py-24`,
+  props.hero && tw`bg-primary-tint-3`,
+  props.valueSection && tw`text-center`,
 ])
 
 const Main = () => {
@@ -13,6 +15,12 @@ const Main = () => {
       <Section hero>
         <Hero />
       </Section>
+      <Section valueSection>
+        <Values />
+      </Section>
+      {/*
+      */}
+
     </Fragment>
   )
 }
