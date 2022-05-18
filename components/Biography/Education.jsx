6a4f887372beea7ef943302fc49f17bf34914ff4
Luis Cadillo <lusicaf3r@Gmail.com>
2022-05-18 10:39:42 -0500

    STYLING: Add education tab

--- components/Biography/Education.jsx
+++ components/Biography/Education.jsx
@@ -1,8 +1,14 @@
 import tw, {css, styled} from 'twin.macro';
+import Card from './Card';
+import {Container} from './style';
 
 const Education = () => {
   return (
-    <h1>Education</h1>
+    <Container>
+      <Card image='hospital' period='1997 - 2003' name='Licenciatura en Medicina y Cirugía' />
+      <Card image='hospital' period='2002 - 2003' name='Foreign Student Exchange Year' />
+      <Card image='hospital' period='2005 - 2007' name='Cursos de Doctorado' />
+    </Container>
   )
 }
 
