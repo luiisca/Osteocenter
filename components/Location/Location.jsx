f14da7a3a9ceff25b51f61f28ac25a444a27bafb
Luis Cadillo <lusicaf3r@Gmail.com>
2022-05-23 21:54:34 -0500

    Add show route on button click

--- components/Location/Location.jsx
+++ components/Location/Location.jsx
@@ -1,14 +1,41 @@
+import {useState} from 'react';
 import tw, {css, styled} from 'twin.macro';
+import {FaRoute} from 'react-icons/fa';
 import {BaseContainer as Container} from '../BaseStyle';
-import {Heading} from '../Elements';
+import {Button, Heading} from '../Elements';
 import Map from '../Map';
 
 const Location = () => {
+  const [userLocation, setUserLocation] = useState(null)
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          setUserLocation(pos);
+        },
+        () => {
+          handleLocationError(true)
+        }
+      )
+    } else {
+      handleLocationError(false)
+    }
+  }
+
+  const handleLocationError = (browserHasGeolocation) => {
+    return browserHasGeolocation ? 'Error: the location service failed' : "Error: your browser doesn't support geolocation"
+  }
+
   return (
     <Container tw='text-center'>
       <Heading as='span' subHeading>Ubicación</Heading>
       <Heading as='h2' secondary>Dónde encontrarnos?</Heading>
-      <Map />
+      <Map userLocation={userLocation}/>
+      <Button type='icon' onClick={getUserLocation}><FaRoute /></Button>
     </Container>
   )
 }
