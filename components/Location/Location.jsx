1938451e6b6b2a2fa809de10dfd93d9cce9bb3e5
LuisCadillo <luiscaf3r@gmail.com>
2022-06-01 01:11:03 -0500

    FEATURE: Migrate map logic from google/wrapper to react-google-maps/api

--- components/Location/Location.jsx
+++ components/Location/Location.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import tw, {css, styled} from 'twin.macro';
-import {Wrapper, Status} from '@googlemaps/react-wrapper';
+import {GoogleMap, LoadScript} from '@react-google-maps/api';
 
 import {BaseContainer as Container} from '../BaseStyle';
 import {Heading} from '../Elements';
@@ -10,6 +10,18 @@ import {Heading} from '../Elements';
 const render = (status) => {
   return <h1>{status}</h1>;
 }
+const StyleGoogleMap = {
+  height: '100%',
+}
+const Map = styled.div(() => [
+  tw`overflow-hidden my-0 mx-auto rounded-2xl w-[900px] h-[500px] transition-all`,
+  css`
+    box-shadow: 1px 1px 10px 0 rgb(116 192 252 / 15%);
+    &:hover {
+      box-shadow: 1px 1px 15px 0 rgb(116 192 252 / 25%);
+    }
+  `,
+])
 
 const Location = () => {
   return (
@@ -17,12 +29,15 @@ const Location = () => {
       <Heading as='span' subHeading>Ubicación</Heading>
       <Heading as='h2' secondary>Dónde encontrarnos?</Heading>
 
-      <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} render={render}>
-        <Map
-          zoom={14}
-          center={{lat: -12.1193972, lng: -77.0339762}}>
+      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} >
+        <Map>
+          <GoogleMap
+            mapContainerStyle={StyleGoogleMap}
+            zoom={14}
+            center={{lat: -12.1193972, lng: -77.0339762}}>
+          </GoogleMap>
         </Map>
-      </Wrapper>
+      </LoadScript>
     </Container>
   )
 }
