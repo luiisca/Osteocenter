acd95430a738a3d842cdf0bc182eed31d06b26ad
LuisCadillo <luiscaf3r@gmail.com>
2022-06-01 01:11:04 -0500

    Add simple marker

--- components/Location/Location.jsx
+++ components/Location/Location.jsx
@@ -1,43 +1,14 @@
-import {useState, useEffect} from 'react';
 import tw, {css, styled} from 'twin.macro';
-import {GoogleMap, LoadScript} from '@react-google-maps/api';
-
 import {BaseContainer as Container} from '../BaseStyle';
 import {Heading} from '../Elements';
-// import Map from '../Map';
-
-
-const render = (status) => {
-  return <h1>{status}</h1>;
-}
-const StyleGoogleMap = {
-  height: '100%',
-}
-const Map = styled.div(() => [
-  tw`overflow-hidden my-0 mx-auto rounded-2xl w-[900px] h-[500px] transition-all`,
-  css`
-    box-shadow: 1px 1px 10px 0 rgb(116 192 252 / 15%);
-    &:hover {
-      box-shadow: 1px 1px 15px 0 rgb(116 192 252 / 25%);
-    }
-  `,
-])
+import Map from '../Map';
 
 const Location = () => {
   return (
     <Container tw='text-center'>
       <Heading as='span' subHeading>Ubicación</Heading>
       <Heading as='h2' secondary>Dónde encontrarnos?</Heading>
-
-      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} >
-        <Map>
-          <GoogleMap
-            mapContainerStyle={StyleGoogleMap}
-            zoom={14}
-            center={{lat: -12.1193972, lng: -77.0339762}}>
-          </GoogleMap>
-        </Map>
-      </LoadScript>
+      <Map />
     </Container>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500&family=Lora&display=swap" rel="stylesheet" />
 
           {/* Calendly */}
