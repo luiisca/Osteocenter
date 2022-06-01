cf017f9ec264196246e38e76d686b829560f8b99
LuisCadillo <luiscaf3r@gmail.com>
2022-06-01 01:11:04 -0500

    FEATURE: Mobile detection based on touchscreen event and userAgent

--- components/Map/Route.jsx
+++ components/Map/Route.jsx
@@ -1,6 +1,7 @@
 import React, {useRef, useState, useCallback, useEffect} from 'react';
 import tw, {css, styled} from 'twin.macro';
 import {Marker, GoogleMap, DirectionsService, DirectionsRenderer, useGoogleMap, useJsApiLoader} from '@react-google-maps/api';
+import {BUSINESS_LOCATION} from '../../static/js/constants';
 
 const Route = ({userLocation}) => {
   const map = useGoogleMap();
@@ -23,34 +24,29 @@ const Route = ({userLocation}) => {
     console.log('REMOVED')
   }
 
-  if (!userLocation) {
-    console.log('no UL');
-    removeDirections();
-  }
-
-
   if (userLocation) {
-    console.log('UL');
     ref.current.directionsService = new google.maps.DirectionsService();
     ref.current.directionsRenderer = new google.maps.DirectionsRenderer();
     ref.current.directionsRenderer.setMap(map);
 
     const request = {
       origin: userLocation,
-      destination: {lat: -12.1193972, lng: -77.0339762},
+      destination: BUSINESS_LOCATION,
       travelMode: 'DRIVING',
     }
 
     ref.current.directionsService.route(request, function (result, status) {
       if (status == 'OK') {
-        ref.current.directionsRenderer.setDirections(result)
+        console.log(result);
+        ref.current.directionsRenderer.setDirections(result);
       }
     })
+  } else {
+    removeDirections();
+    return (
+      <Marker position={{lat: -12.1193972, lng: -77.0339762}} />
+    )
   }
-
-  return (
-    <Marker position={{lat: -12.1193972, lng: -77.0339762}} />
-  )
 }
 
 export default Route
