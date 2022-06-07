af5a4b104a891ccb9aa03b1f1c92b4796376a68a
LuisCadillo <luiscaf3r@gmail.com>
2022-06-06 19:48:18 -0500

    STYLING: CTA section

---
+++ components/CallToAction/index.js
@@ -0,0 +1,2 @@
+export * from './CallToAction';
+export {default} from './CallToAction';
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ import Layout, {siteTitle} from '../components/Layout';
 import Header from '../components/Sections/header';
 import Main from '../components/Sections/main';
 import Footer from '../components/Sections/footer';
-import Test from '../components/TestDetailsLayout/Test';
-import TestProvider from '../components/TestDetailsLayout/TestProvider';
 
 export default function Home() {
   return (
