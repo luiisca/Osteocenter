2fe41c9f7e0b088f51807c7f13d6e083f40ed458
LuisCadillo <luiscaf3r@gmail.com>
2022-06-08 11:55:58 -0500

    FEATURE: Articles Carousel

--- static/js/constants.js
+++ static/js/constants.js
@@ -5,3 +5,6 @@ export const LIBRARIES = ["places"];
 export const MAP_DIRECTIONS = (userLocation) => `https://www.google.com/maps/dir/${userLocation?.lat},${userLocation?.lng}/${BUSINESS_LOCATION.lat},${BUSINESS_LOCATION.lng}`
 
 export const TOTAL_STARS = 5;
+
+// carousel
+export const ARTICLES_PER_PAGE = 2;
