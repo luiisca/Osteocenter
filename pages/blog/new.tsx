bab11b4cf50659e3427f9c8093379df24e38bff4
LuisCadillo <luiscaf3r@gmail.com>
2022-07-01 12:40:17 -0500

    CONFIG: Types + basic editor

--- pages/blog/new.tsx
+++ pages/blog/new.tsx
@@ -1,13 +1,50 @@
+import { styled } from "twin.macro";
 import { gql } from "@apollo/client";
-import Layout from "../../components/Layout";
-import { Button, Heading } from "../../components/Elements";
 import {
-  ArticleDocument,
   useCreateArticleMutation,
   usePublishArticleMutation,
 } from "../../generated";
-import type { CreateArticleMutation } from "../../generated";
+import { useState, useMemo } from "react";
+import { createEditor, Descendant } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+
 import { FiEdit } from "react-icons/fi";
+
+import Layout from "../../components/Layout";
+import { Button, Heading } from "../../components/Elements";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 100px;
+`;
+
+// Create our initial value...
+const initialValue: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: "This example shows how you can make a hovering menu appear above your content, which you can use to make text ",
+      },
+      { text: "bold", bold: true },
+      { text: ", " },
+      { text: ", or anything else you might want to do!" },
+    ],
+  },
+  {
+    type: "paragraph",
+    children: [
+      { text: "Try it out yourself! Just " },
+      { text: "select any piece of text and the menu will appear", bold: true },
+      { text: "." },
+    ],
+  },
+];
+
 // temp
 const newArticleData = {
   slug: "new-article-slug-699",
@@ -141,13 +178,12 @@ const newArticleData = {
 };
 
 const NewArticle = () => {
-  // TODO:
-  // 1. When a new article is created, is it cached? I know it pushes new data to the GraqhQL server. The docs say they're cached but not referenced by a list fields hence I got to manually reference them with update(). But writeQuery() only perform local changes (all reset on reload), that this mean that the referencing process for list fields is done after each new request??
-  // 2. Look up at the cache and test wether a new reference is added to the Articles field after writeFragment() or if its as I guess and the whole object is added but is then replaced by a reference after ISR hits.
-  // https://www.apollographql.com/docs/react/caching/cache-interaction#values-vs-references. According to this, if I create a new article, that should be added as a reference to the Articles field and not as the actual object, now if I look at the cache after a mutation I should see an Articles field containing a new reference, not a new object.
+  const [value, setValue] = useState<Descendant[]>(initialValue);
+  const editor = useMemo(() => withReact(createEditor()), []);
 
   const [createArticleMutation, { loading: createLoading }] =
     useCreateArticleMutation();
+
   const [
     publishArticleMutation,
     { loading: publishLoading, error: publishError },
@@ -156,7 +192,7 @@ const NewArticle = () => {
   const createArticle = async (): Promise<void> => {
     const { data } = await createArticleMutation({
       variables: newArticleData,
-      // add new reference to articles object on apollo cache as it doesn't it automatically
+      // add new reference to articles list fields on apollo cache as it doesn't do that automatically
       update: (cache, mutationResult) => {
         const { data } = mutationResult;
         cache.modify({
@@ -203,6 +239,17 @@ const NewArticle = () => {
       {publishLoading && <p>Publicando articulo</p>}
       {publishError && <p>Hubo un problema, reintentar</p>}
       <Heading tertiary>New Article</Heading>
+
+      <Container>
+        <Slate
+          editor={editor}
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        >
+          <Editable />
+        </Slate>
+      </Container>
+
       <Button elType="text" cta hero href="#" tw="mr-4" onClick={createArticle}>
         <FiEdit />
         Crear
