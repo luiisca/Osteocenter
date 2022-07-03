ee653fc9946cfa17034eb2959e48e61643aa6d6f
LuisCadillo <luiscaf3r@gmail.com>
2022-07-02 22:01:55 -0500

    FEATURE: Add image uploader to add new article form

--- pages/blog/new.tsx
+++ pages/blog/new.tsx
@@ -4,21 +4,38 @@ import {
   useCreateArticleMutation,
   usePublishArticleMutation,
 } from "../../generated";
-import { useState, useMemo } from "react";
+import { useState, useMemo, useRef } from "react";
 import { createEditor, Descendant } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
+import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from "formik";
 
 import { FiEdit } from "react-icons/fi";
 
 import Layout from "../../components/Layout";
 import { Button, Heading } from "../../components/Elements";
 
+interface MyFormValues {
+  title: string;
+  excerpt: string;
+  featured: boolean;
+  featuredImage: null;
+  featuredImagePreview: string;
+}
+
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
-  align-items: center;
+  text-align: center;
+  flex-direction: column;
   padding: 0 100px;
 `;
 
@@ -44,7 +61,6 @@ const initialValue: Descendant[] = [
     ],
   },
 ];
-
 // temp
 const newArticleData = {
   slug: "new-article-slug-699",
@@ -179,6 +195,7 @@ const newArticleData = {
 
 const NewArticle = () => {
   const [value, setValue] = useState<Descendant[]>(initialValue);
+  const [testState, setTestState] = useState<string>("hello test");
   const editor = useMemo(() => withReact(createEditor()), []);
 
   const [createArticleMutation, { loading: createLoading }] =
@@ -189,50 +206,75 @@ const NewArticle = () => {
     { loading: publishLoading, error: publishError },
   ] = usePublishArticleMutation();
 
-  const createArticle = async (): Promise<void> => {
-    const { data } = await createArticleMutation({
-      variables: newArticleData,
-      // add new reference to articles list fields on apollo cache as it doesn't do that automatically
-      update: (cache, mutationResult) => {
-        const { data } = mutationResult;
-        cache.modify({
-          fields: {
-            articles(existingArticles = []) {
-              const newArticleRef = cache.writeFragment({
-                data: data?.createArticle,
-                fragment: gql`
-                  fragment NewArticle on articles {
-                    id
-                    slug
-                    title
-                    excerpt
-                    content {
-                      raw
-                      markdown
-                      html
-                    }
-                    featuredPost
-                    featuredImage {
-                      width
-                      height
-                      url
-                    }
-                  }
-                `,
-              });
-              return [...existingArticles, newArticleRef];
-            },
-          },
-        });
-      },
-    });
-    publishArticleMutation({
-      variables: {
-        id: data?.createArticle?.id,
-      },
-    });
+  const handleImagePreview = (values: any, setFieldValue: any) => {
+    if (values.featuredImage) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFieldValue("featuredImagePreview", reader.result);
+        console.log("onLoaded render", reader.result);
+      };
+      console.log("handleImagePreview", values);
+      reader.readAsDataURL(values.featuredImage);
+    }
   };
 
+  const createArticle = (values: any, actions: any) => {
+    console.log(values, actions);
+    const form = new FormData();
+    form.append("featuredImage", values.featuredImage);
+    for (let p of form) {
+      console.log(p);
+    }
+    // const { data } = await createArticleMutation({
+    //   variables: newArticleData,
+    //   // add new reference to articles list fields on apollo cache as it doesn't do that automatically
+    //   update: (cache, mutationResult) => {
+    //     const { data } = mutationResult;
+    //     cache.modify({
+    //       fields: {
+    //         articles(existingArticles = []) {
+    //           const newArticleRef = cache.writeFragment({
+    //             data: data?.createArticle,
+    //             fragment: gql`
+    //               fragment NewArticle on articles {
+    //                 id
+    //                 slug
+    //                 title
+    //                 excerpt
+    //                 content {
+    //                   raw
+    //                   markdown
+    //                   html
+    //                 }
+    //                 featuredPost
+    //                 featuredImage {
+    //                   width
+    //                   height
+    //                   url
+    //                 }
+    //               }
+    //             `,
+    //           });
+    //           return [...existingArticles, newArticleRef];
+    //         },
+    //       },
+    //     });
+    //   },
+    // });
+    // publishArticleMutation({
+    //   variables: {
+    //     id: data?.createArticle?.id,
+    //   },
+    // });
+  };
+
+  const initialValues: MyFormValues = {
+    title: "",
+    excerpt: "",
+    featured: true,
+    featuredImage: null,
+    featuredImagePreview: "",
+  };
   return (
     <Layout>
       {createLoading && <p>Creando articulo</p>}
@@ -240,20 +282,72 @@ const NewArticle = () => {
       {publishError && <p>Hubo un problema, reintentar</p>}
       <Heading tertiary>New Article</Heading>
 
+      <Formik initialValues={initialValues} onSubmit={createArticle}>
+        {({ values, setFieldValue }: FormikProps<MyFormValues>) => (
+          <Form>
+            <div>
+              <label htmlFor="title">Titulo</label>
+              <Field id="title" name="title" required />
+            </div>
+            <div>
+              <label htmlFor="excerpt">Extracto</label>
+              <Field name="excerpt" id="excerpt" required />
+            </div>
+            <div id="feature-article">Destacar articulo</div>
+            <div role="group" aria-labelledby="feature-article">
+              <label>
+                <Field type="radio" name="featured" value="yes" />
+                Yes
+              </label>
+              <label>
+                <Field type="radio" name="featured" value="no" />
+                No
+              </label>
+              <p>Seleccionado: {values.featured}</p>
+            </div>
+            <input
+              type="file"
+              id="file"
+              name="file"
+              onChange={(e: any) => {
+                console.log("input file on change", e.currentTarget.files[0]);
+                setTestState("bye");
+                console.log(testState);
+                // it is not possible to access the new value of state inmediately after defining it, gotta wait to the next render
+                setFieldValue("featuredImage", e.currentTarget.files[0]);
+              }}
+            />
+            {handleImagePreview(values, setFieldValue)}
+            {values.featuredImage && (
+              <img src={values.featuredImagePreview} alt="preview image" />
+            )}
+            <Button elType="submit" type="submit" cta hero tw="mr-4">
+              <FiEdit />
+              Publicar
+            </Button>
+          </Form>
+        )}
+      </Formik>
+      {/*
       <Container>
         <Slate
           editor={editor}
           value={value}
-          onChange={(newValue) => setValue(newValue)}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
         >
-          <Editable />
+          <Editable
+            placeholder="Enter some text..."
+            onKeyDown={(event: React.KeyboardEvent) => {
+              if (event.key !== "&") return;
+              event.preventDefault();
+              editor.insertText("and");
+            }}
+          />
         </Slate>
       </Container>
-
-      <Button elType="text" cta hero href="#" tw="mr-4" onClick={createArticle}>
-        <FiEdit />
-        Crear
-      </Button>
+      */}
     </Layout>
   );
 };
