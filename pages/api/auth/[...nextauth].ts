5263a5bfc218e29a485cdbbecebbd062af5c2fca
LuisCadillo <luiscaf3r@gmail.com>
2022-08-08 12:15:28 -0500

    Remove Twitter auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 // import GoogleProvider from "next-auth/providers/google";
-import TwitterProvider from "next-auth/providers/twitter";
 // import FacebookProvider from "next-auth/providers/facebook";
 import GithubProvider from "next-auth/providers/github";
 
@@ -13,10 +12,6 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID || "",
-      clientSecret: process.env.TWITTER_SECRET || "",
     }),
     // GoogleProvider({
     //   clientId: process.env.GOOGLE_ID,
