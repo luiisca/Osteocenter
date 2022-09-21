4ba18977828454f49446ed8f3aa1668154c4ed49
LuisCadillo <luiscaf3r@gmail.com>
2022-09-20 19:38:25 -0500

    change favicon

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -36,36 +36,54 @@ const SEO = ({
         {keywords && <meta content={keywords} name="keywords" />}
         <meta content="follow, index" name="robots" />
         {/* <--- Favicons ---> */}
-        <meta name="msapplication-TileColor" content="#2b5797" />
-        <meta name="theme-color" content="#f1f9ff" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
+          href="https://osteocenter.vercel.app/favicons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicons/favicon-32x32.png"
+          href="https://osteocenter.vercel.app/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="194x194"
+          href="https://osteocenter.vercel.app/favicons/favicon-194x194.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="https://osteocenter.vercel.app/favicons/android-chrome-192x192.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicons/favicon-16x16.png"
+          href="https://osteocenter.vercel.app/favicons/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="manifest"
+          href="https://osteocenter.vercel.app/favicons/site.webmanifest"
+        />
         <link
           rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
+          href="https://osteocenter.vercel.app/favicons/safari-pinned-tab.svg"
           color="#5bbad5"
         />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link
+          rel="shortcut icon"
+          href="https://osteocenter.vercel.app/favicons/favicon.ico"
+        />
+        <meta name="msapplication-TileColor" content="#2b5797" />
         <meta
           name="msapplication-config"
-          content="/favicons/browserconfig.xml"
+          content="https://osteocenter.vercel.app/favicons/browserconfig.xml"
         />
+        <meta name="theme-color" content="#f1f9ff" />
         {/* <--- Open Graph ---> */}
         <meta content="es_PE" property="og:locale" />
         <meta content={title} property="og:title" />
